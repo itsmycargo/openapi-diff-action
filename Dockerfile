b70186d4dd8fbbf7599f6b2b9c095d0f11879a8f
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2021-03-03 20:18:26 +0100

    fix: running action

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM openapitools/openapi-diff:latest
 
+RUN apt-get update \
+  && apt-get install -y --no-install-recommends curl jq \
+  && rm -rf /var/lib/apt/lists/*
+
 COPY entrypoint.sh /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,42 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
+set -Eeuo pipefail
 
-OLD_SCHEMA=$1; shift
-NEW_SCHEMA=$1; shift
-CHANGES_OUTPUT=$1; shift
+BASE_SCHEMA=$1
+REVISION_SCHEMA=$2
 
-state=$(
+STATE=$(
   java -jar /app/openapi-diff.jar \
-  --markdown "${{ github.workspace }}/${CHANGES_OUTPUT}" \
-  --state \
-  "${OLD_SCHEMA}" \
-  "${{ github.workspace }}/${NEW_SCHEMA}"
+    --text changes.txt \
+    --markdown changes.md \
+    --state \
+    "${BASE_SCHEMA}" \
+    "${REVISION_SCHEMA}"
 )
-echo "::set-output name=state::${state}"
-echo "::debug::API Schema status: ${state}"
+
+
+echo "::group::API Changes"
+cat changes.txt
+echo ""
+echo "::endgroup::"
+
+CHANGES=$(cat changes.md)
+CHANGES="${CHANGES//'%'/'%25'}"
+CHANGES="${CHANGES//$'\n'/'%0A'}"
+CHANGES="${CHANGES//$'\r'/'%0D'}"
+
+VERDICT=""
+case "${STATE}" in
+  no_changes )
+    VERDICT=":white_check_mark: No Changes."
+    ;;
+  incompatible )
+    VERDICT=":bangbang: Incompatible Changes :bangbang:"
+    ;;
+  compatible )
+    VERDICT=":white_check_mark: Only compatible changes."
+    ;;
+esac
+
+echo "::set-output name=changes::${CHANGES}"
+echo "::set-output name=state::${STATE}"
+echo "::set-output name=verdict::${VERDICT}"
